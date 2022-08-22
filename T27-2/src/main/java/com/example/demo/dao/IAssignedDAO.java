29f59db71739d089c4e76573d1393062767237b9
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-22 10:59:59 +0200

    Delete T27-2 directory

--- T27-2/src/main/java/com/example/demo/dao/IAssignedDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Assigned;
-
-public interface IAssignedDAO extends JpaRepository<Assigned, Long>{
-
-}
